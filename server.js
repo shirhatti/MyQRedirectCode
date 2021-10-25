e991809d718289b0624de522cdb47a59eb88269a
Brian Beaird <brbeaird@gmail.com>
2021-10-25 09:17:21 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+//create the epxress app
+var express = require('express');
+var app = express();
+const axios = require('axios');
+var port = process.env.PORT || 8180;
+
+app.use(express.json());
+
+app.get('/', function (req, res) {
+  res.send("Server Up!")
+});
+
+/**
+ * This method takes in an authenticated cookie and redirect URL from the MyQ OAuth process.
+ * It then passes that cookie along in a GET request to that URL.
+ * Finally, it grabs the resulting code and scope from the resulting location header.
+ * Note that without the original PKCS challenge information, this method itself it not able
+ * to fully generate MyQ login information.
+ */
+app.post('/getRedirectCode', async function (req, res) {
+  try {
+    let config =
+    {
+      headers: {
+        "Cookie": req.body.cookie,
+        "User-Agent": "null"
+      },
+      maxRedirects: 0
+    }
+    try {
+      await axios.get(req.body.redirectUrl, config);
+    } catch (error) {
+      if (error.response?.status == 302){
+        const redirectResponseUrl = new URL(error.response?.headers['location'] ?? "");
+        let result = {
+          code: redirectResponseUrl.searchParams.get("code"),
+          scope: redirectResponseUrl.searchParams.get("scope")
+        }
+        res.json(result);
+        return;
+      }
+      else{
+        res.status(error.response?.status ? error.response.status : 500).json({error: error.message});
+        return;
+      }
+    }
+    res.status(500).json({error: "Failed to get code from MyQ"});
+
+  } catch (error) {
+    console.error(error.stack);
+    res.status(500).json({error: error.message});
+  }
+})
+
+//listen on the port
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`)
+})
+
+//export the app module
+exports = module.exports = app;
